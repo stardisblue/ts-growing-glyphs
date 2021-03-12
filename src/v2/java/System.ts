9bd8920c66a49e880b02a52183afa27c553a81a1
stardisblue <stardisblue@gmail.com>
2021-03-12 17:17:20 +0100

    transcription done

---
+++ src/v2/java/System.ts
@@ -0,0 +1,13 @@
+export class System {
+    static out = {
+        println(...strs: string[]) {
+            console.log(...strs);
+        }
+    };
+
+    static err = {
+        println(...strs: string[]) {
+            console.error(...strs);
+        }
+    };
+}
